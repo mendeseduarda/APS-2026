e4fa4190796ffe20702077af86d0e8ee1f07adad
mendeseduarda <mendeseduardamaria1@gmail.com>
2026-06-06 14:56:51 -0300

    chore: exclusao de pastas sem utilidade e mudanca no readme

--- backend/Managers/PedidoManager.h
+++ backend/Managers/PedidoManager.h
@@ -13,16 +13,21 @@ class PedidoManager {
         void informaAdicaoGas(
             bool adicionaGas
         );
+
         void informaModalidade(
             bool modalidade //troca ou nao 
         );
+
         void iniciaFinalizacaoPedido();
+
         void insereCupom(
             int CodigoPromocional
         );
+
         void confirmaEndereco(
             const Endereco& endereco
         );
+        
         void informaPagamento(
             const std::string& metodoPagamento
         );
